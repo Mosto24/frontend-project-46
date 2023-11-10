0c2a8950e22cf8030a725eae47c84cf8c19d23a2
Mosto <prikol.mosto@gmail.com>
2023-11-10 22:08:18 +0700

    Second Step. Add two filepath, and update -h option.

--- commander.js
+++ commander.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
-import {Command} from 'commander'; // (normal include)
+// import {Command} from 'commander'; // (normal include)
 import {program} from 'commander';
 
 program
   .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>',  'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.');
   
 
