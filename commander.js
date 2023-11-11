78104a15033736ab8f282ab634ca9973fec97121
Mosto <prikol.mosto@gmail.com>
2023-11-11 15:49:25 +0700

    Step four. Create gitignore, make takeAndEcho function.

--- commander.js
+++ commander.js
@@ -3,18 +3,41 @@
 // import {Command} from 'commander'; // (normal include)
 import {program} from 'commander';
 import { readFileSync } from 'node:fs';
+import _ from 'lodash';
+
+function takeAndEcho(filepath1, filepath2) {
+  const f1 = JSON.parse(readFileSync(filepath1, "utf8"));
+  const f2 = JSON.parse(readFileSync(filepath2, "utf8"));
+  const arrF1 = Object.keys(f1);
+  const arrF2 = Object.keys(f2);
+  let resultObj = {};
+  let arrF2AndF1keys = [];
+  for (let key of arrF1) {
+    if(arrF2.includes(key)) {
+      if(f1[key] == f2[key]) {
+        resultObj[key] = f2[key];
+      } else {
+        resultObj[`- ${key}`] = f1[key];
+        resultObj[`+ ${key}`] = f2[key];
+      }
+    } else {
+      resultObj[`- ${key}`] = f1[key];
+    }
+  }
+  for (let key of arrF2) {
+    if (!arrF1.includes(key)) {
+      resultObj[`+ ${key}`] = f2[key];
+    }
+  }
+  console.log(JSON.stringify(resultObj));
+  }
 
 program
   .option('-V, --version', 'output the version number')
   .option('-f, --format <type>',  'output format')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .action((filepath1, filepath2) => {
-    let f1 = readFileSync(filepath1, "utf8");
-    console.log(JSON.parse(f1));
-    let f2 = readFileSync(filepath2, "utf8");
-    console.log(JSON.parse(f2));
-  });
+  .action(takeAndEcho);
   
 
 program.parse(process.argv);
