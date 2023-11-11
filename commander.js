c3969acf78087e002bc52ca68c64e095f56a346f
Mosto <prikol.mosto@gmail.com>
2023-11-11 12:50:39 +0700

    Step three. Add to filepath arguments and parse the from JSON.

--- commander.js
+++ commander.js
@@ -2,13 +2,19 @@
 
 // import {Command} from 'commander'; // (normal include)
 import {program} from 'commander';
+import { readFileSync } from 'node:fs';
 
 program
   .option('-V, --version', 'output the version number')
   .option('-f, --format <type>',  'output format')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .description('Compares two configuration files and shows a difference.');
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .action((filepath1, filepath2) => {
+    let f1 = readFileSync(filepath1, "utf8");
+    console.log(JSON.parse(f1));
+    let f2 = readFileSync(filepath2, "utf8");
+    console.log(JSON.parse(f2));
+  });
   
 
 program.parse(process.argv);
