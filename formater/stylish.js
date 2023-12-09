aa744fefa86e0d0548e2a5502904b9c35ff9854b
Mosto <prikol.mosto@gmail.com>
2023-12-09 08:42:11 +0700

    Fix.

--- formater/stylish.js
+++ formater/stylish.js
@@ -31,20 +31,20 @@ function deep(file1, file2) {
     for (let key of arrF1) {
       if(arrF2.includes(key)) {
         if(typeof f1[key] == 'object' && typeof f2[key] == 'object') {
-          resultObj[`  ${key}`] = recurse(f1[key], f2[key]);
+          resultObj[`    ${key}`] = recurse(f1[key], f2[key]);
         } else if (f1[key] != f2[key]) {
-          resultObj[`- ${key}`] = f1[key];
-          resultObj[`+ ${key}`] = f2[key];
+          resultObj[` - ${key}`] = f1[key];
+          resultObj[` + ${key}`] = f2[key];
         } else {
-          resultObj[`  ${key}`] = f2[key];
+          resultObj[`    ${key}`] = f2[key];
         }
       } else {
-        resultObj[`- ${key}`] = f1[key];
+        resultObj[` - ${key}`] = f1[key];
       }
     }
     for(let key of arrF2) {
       if(!arrF1.includes(key)) {
-        resultObj[`+ ${key}`] = f2[key];
+        resultObj[` + ${key}`] = f2[key];
       }
     }
     function recurse(obj1, obj2) {
@@ -52,20 +52,20 @@ function deep(file1, file2) {
       for (let key in obj1) {
         if (Object.hasOwn(obj2, key)) {
           if(typeof obj1[key] == 'object' && typeof obj2[key] == 'object') {
-            iterValue[`  ${key}`] = recurse(obj1[key], obj2[key]);
+            iterValue[`    ${key}`] = recurse(obj1[key], obj2[key]);
           } else if (obj1[key] != obj2[key]) {
-            iterValue[`- ${key}`] = obj1[key];
-            iterValue[`+ ${key}`] = obj2[key];
+            iterValue[` - ${key}`] = obj1[key];
+            iterValue[` + ${key}`] = obj2[key];
           } else {
-            iterValue[`  ${key}`] = obj2[key];
+            iterValue[`   ${key}`] = obj2[key];
           }
         } else {
-          iterValue[`- ${key}`] = obj1[key];
+          iterValue[` - ${key}`] = obj1[key];
         }
       }
       for(let key in obj2) {
         if(!Object.hasOwn(obj1, key)) {
-          iterValue[`+ ${key}`] = obj2[key];
+          iterValue[` + ${key}`] = obj2[key];
         }
       }
       return iterValue;
